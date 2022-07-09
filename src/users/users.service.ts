19d054af205a4550723bc1e4b12d171e95c49d0e
garyjeong <smat91@naver.com>
2022-07-09 17:42:48 +0900

    ➕ 서비스 정의

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,4 +1,42 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { UsersRepository } from 'src/database/repositories/users.repository';
+import { Users } from '../database/entities/users.entity';
 
 @Injectable()
-export class UsersService {}
+export class UsersService {
+	constructor(
+		@InjectRepository(Users)
+		private usersRepository: UsersRepository,
+	) {}
+
+    /**
+     * 계정 생성
+     */
+    SignUp() {}
+
+    /**
+     * 로그인
+     */
+	SignIn() {}
+
+    /**
+     * 사용자 정보 가져오기
+     */
+    GetOne() {}
+
+    /**
+     * 사용자 상세 정보 가져오기(닉네임 등)
+     */
+    GetOneDetail() {}
+
+    /**
+     * 사용자 정보 변경
+     */
+    Update() {}
+
+    /**
+     * 회원탈퇴
+     */
+    Delete() {}
+}
